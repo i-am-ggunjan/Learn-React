19a2808a9d1f9ab002360caa74b1fc16da82a5d3
i-am-ggunjan <gauravgunjan200301@gmail.com>
2023-03-01 00:14:45 +0530

    Establish Connection between index.js and App.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
 //! ================React================== !//
 // let ele = React.createElement("p", { className: "main" }, "Demo");
@@ -10,9 +11,12 @@ import ReactDOM from "react-dom/client";
 // ReactDOM.createRoot(document.getElementById("root")).render(ele);
 
 //! ==================JSX================== !//
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <p className="main">Demo</p>
-);
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <p className="main">Demo</p>
+// );
 
 // ReactDOM.createRoot(document.getElementById("root")).render(<P className='main'>Demo</P>);
 // ? It will give an error --> 'P' is not defined /// In JSX, tag should be in lower case. If capital then it will treat as component.
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
